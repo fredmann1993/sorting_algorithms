287c231af33981ecfcbbdfb8b791eb352a50986a
Fisayo <fasuyifisayo@gmail.com>
2023-02-23 12:03:36 +0100

    committed

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,56 +1,43 @@
-#include "sort.h"
-
 /**
-  * shell_sort - A function tat sorts an array usin shell algorithm.
-  * @array: The array to sort.
-  * @size: The length of the array.
-  * Return: Nothing.
-  */
-void shell_sort(int *array, size_t size) {
-    int interval = 1;
-    while (interval < size / 3) {
-        interval = interval * 3 + 1; // calculate Knuth sequence interval
-    }
+* shell_sort - sorts an array of integers in ascending order using the
+*              Shell sort algorithm, using the Knuth sequence
+*
+* @array: Pointer to the array to sort
+* @size:  Size of the array
+*/
+void shell_sort(int *array, size_t size)
+{
+size_t gap = 1, i, j;
+int tmp;
 
-    while (interval > 0) {
-        printf("Array after interval %d sorting: ", interval);
-        for (int i = interval; i < size; i++) {
-            int temp = array[i];
-            int j = i;
-            while (j >= interval && array[j - interval] > temp) {
-                array[j] = array[j - interval];
-                j -= interval;
-            }
-            array[j] = temp;
-        }
-
-        // decrease the interval
-        interval = (interval - 1) / 3;
-        // print the current state of array
-        for (int i = 0; i < size; i++) {
-            printf("%d ", array[i]);
-        }
-        printf("\n");
-    }
+/* Calculate the initial gap */
+while (gap < size)
+{
+gap = gap * 3 + 1;
 }
 
-int main() {
-    int arr[] = {45, 23, 76, 32, 82, 15, 68, 53};
-    size_t size = sizeof(arr) / sizeof(arr[0]);
+while (gap > 0)
+{
+/* Do the insertion sort for the current gap */
+for (i = gap; i < size; i++)
+{
+tmp = array[i];
 
-    printf("Array before sorting: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+{
+array[j] = array[j - gap];
+}
 
-    shell_sort(arr, size);
+array[j] = tmp;
+}
 
-    printf("Array after sorting: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+/* Calculate the next gap and print the array */
+gap = (gap - 1) / 3;
 
-    return 0;
+if (gap % 4 == 0 && gap > 0)
+{
+printf("Array after sorting with gap %lu: ", gap);
+print_array(array, size);
+}
+}
 }
